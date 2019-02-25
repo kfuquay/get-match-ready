b560950bfa84845ecbfdfc0c37dce68455865011
kfuquay <kirsty.fuquay@gmail.com>
2019-02-25 09:50:02 -0500

    add comments to script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//GLOBAL VARIABLES
+//GLOBAL VARIABLES/STATE
 const fdOptions = {
   headers: new Headers({
     "X-Auth-Token": "a2d6e538e4b746ecafde70ea5964cdb0"
@@ -8,6 +8,7 @@ const fdOptions = {
 };
 
 // UTC DATE FORMATTER
+//converts utc formatted date response to more readable format
 function dateFormatter(UTC) {
   const dateFormat = "YYYY-MM-DD / hh:mma";
 
@@ -15,6 +16,7 @@ function dateFormatter(UTC) {
 }
 
 // RENDER/DISPLAY
+//finds next scheduled match for user selected team sends utc date to dateFormatter function, empties container, populates container with info about next match, sends id of homeTeam to getVenue function
 function displayNextMatch(responseJson) {
   const nextMatch = jQuery.grep(responseJson.matches, function(item) {
     return item.status === "SCHEDULED";
@@ -46,51 +48,46 @@ function displayNextMatch(responseJson) {
   getVenue(homeTeam);
 }
 
+//displays stadium name for user selected team's next match (takes response from getVenue as param)
 function displayNextMatchVenue(responseJson) {
   $(".next-match-container").append(`
         <p class="next-match"><span>Stadium:</span> ${responseJson.venue}</p>
     `);
 }
 
+//empties container, displays results from requestStats function
 function displayStats(responseJson, userTeam) {
   $(".js-container").empty();
 
   for (let i = 0; i < responseJson.standings[0].table.length; i++) {
     if (userTeam == responseJson.standings[0].table[i].team.id) {
       $(".js-container").append(`
-                <h2>Stats</h2>
-                <ul id="stats">
-                    <li>League Position: ${
-                      responseJson.standings[0].table[i].position
-                    }</li>
-                    <li>Played Games: ${
-                      responseJson.standings[0].table[i].playedGames
-                    }</li>
-                    <li>Won Games: ${
-                      responseJson.standings[0].table[i].won
-                    }</li>
-                    <li>Drawn Games: ${
-                      responseJson.standings[0].table[i].draw
-                    } </li>
-                    <li>Lost Games: ${
-                      responseJson.standings[0].table[i].lost
-                    }</li>
-                    <li>Goals For: ${
-                      responseJson.standings[0].table[i].goalsFor
-                    }</li>
-                    <li>Goals Against: ${
-                      responseJson.standings[0].table[i].goalsAgainst
-                    }</li>
-                    <li>Goal Difference: ${
-                      responseJson.standings[0].table[i].goalDifference
-                    }</li>
-                </ul>
-                
-            `);
+          <h2>Stats</h2>
+          <ul id="stats">
+              <li>League Position: ${
+                responseJson.standings[0].table[i].position
+              }</li>
+              <li>Played Games: ${
+                responseJson.standings[0].table[i].playedGames
+              }</li>
+              <li>Won Games: ${responseJson.standings[0].table[i].won}</li>
+              <li>Drawn Games: ${responseJson.standings[0].table[i].draw} </li>
+              <li>Lost Games: ${responseJson.standings[0].table[i].lost}</li>
+              <li>Goals For: ${responseJson.standings[0].table[i].goalsFor}</li>
+              <li>Goals Against: ${
+                responseJson.standings[0].table[i].goalsAgainst
+              }</li>
+              <li>Goal Difference: ${
+                responseJson.standings[0].table[i].goalDifference
+              }</li>
+          </ul>
+          
+      `);
     }
   }
 }
 
+//empties container, populates with results from news API
 function displayNews(newsResponseJson) {
   $(".js-container").empty();
   $(".js-container").append(`
@@ -111,11 +108,17 @@ function displayNews(newsResponseJson) {
   }
 }
 
+//hides intro paragraph, empties container, displays user selected team's crest or if crest url is invalid displays football image/logo, displays nav with ARIA-live attribute of 'polite'
 function displayTeam(responseJson) {
   $(".intro-container").addClass("hidden");
   $(".js-container").empty();
-  
-  if (responseJson.crestUrl !== null && responseJson.tla !== 'BRI' && responseJson.tla !== 'WBA' && responseJson.tla !== 'HUL') {
+
+  if (
+    responseJson.crestUrl !== null &&
+    responseJson.tla !== "BRI" &&
+    responseJson.tla !== "WBA" &&
+    responseJson.tla !== "HUL"
+  ) {
     $(".js-container").append(`
             <img class="crest" alt="selected team's crest" src="${
               responseJson.crestUrl
@@ -125,11 +128,12 @@ function displayTeam(responseJson) {
     $(".js-container").append(`
       <i class="fas fa-futbol"></i>`);
   }
-  //show team nav (news, stats, next match)
+  //display nav
   $(".nav").removeClass("hidden");
   $(".nav").attr("aria-live", "polite");
 }
 
+//inserts teams competing in current season of Championship league as options in select dropdown element(sorted alphabetically)
 function insertCurrentTeams(responseJson) {
   const currentTeams = [];
 
@@ -158,6 +162,7 @@ function insertCurrentTeams(responseJson) {
 }
 
 // API CALLS
+//get map to display - takes response from getVenueLocation as param
 function getMap(responseJson) {
   const lat = responseJson[0].lat;
   const long = responseJson[0].lon;
@@ -179,6 +184,26 @@ function getMap(responseJson) {
   L.marker([`${lat}`, `${long}`]).addTo(map);
 }
 
+//get lat and long of venue/stadium(takes venue address as param) - send response to getMap function
+function getVenueLocation(responseJson) {
+  const apikey = "7759afc20a6964";
+  const venue = responseJson.address;
+  const URL = `https://eu1.locationiq.com/v1/search.php?key=${apikey}&q=${venue}&format=json`;
+
+  fetch(URL)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => getMap(responseJson))
+    .catch(err => {
+      alert(`Something went wrong: ${err.message}`);
+    });
+}
+
+//get venue of user selected team's next match - send response to displayNextMatchVenue and getVenueLocation
 function getVenue(homeTeam) {
   const URL = `https://api.football-data.org/v2/teams/${homeTeam}
     `;
@@ -199,24 +224,7 @@ function getVenue(homeTeam) {
     });
 }
 
-function getVenueLocation(responseJson) {
-  const apikey = "7759afc20a6964";
-  const venue = responseJson.address;
-  const URL = `https://eu1.locationiq.com/v1/search.php?key=${apikey}&q=${venue}&format=json`;
-
-  fetch(URL)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => getMap(responseJson))
-    .catch(err => {
-      alert(`Something went wrong: ${err.message}`);
-    });
-}
-
+//get info about user selected team's next match - send response to displayNextMatch function
 function requestNextMatch(userTeam) {
   const URL = `https://api.football-data.org/v2/teams/${userTeam}/matches`;
 
@@ -233,6 +241,7 @@ function requestNextMatch(userTeam) {
     });
 }
 
+//get current seasons stats for user selected team - send response to displayStats function
 function requestStats(userTeam) {
   const URL = `https://api.football-data.org/v2/competitions/2016/standings`;
 
@@ -249,6 +258,7 @@ function requestStats(userTeam) {
     });
 }
 
+//get news about user selected team - send response to displayNews function
 function requestTeamNews(userTeamName) {
   const newsapiKey = "80fc5c87ac064513a95dc5d37927c2dc";
 
@@ -273,6 +283,7 @@ function requestTeamNews(userTeamName) {
     });
 }
 
+//get info about user selected team - send response to displayTeam function
 function requestTeam(userTeam) {
   const URL = `https://api.football-data.org/v2/teams/${userTeam}`;
 
@@ -289,6 +300,7 @@ function requestTeam(userTeam) {
     });
 }
 
+//get list of teams competing in Championship league for current season - send response to insertCurrentTeams function
 function getCurrentTeams() {
   const URL = "https://api.football-data.org/v2/competitions/2016/teams";
 
@@ -330,13 +342,16 @@ function watchNewsButton() {
 function watchForm() {
   $(".js-select-team").change(function() {
     const userTeam = $(".js-select-team").val();
-    // $('.header').addClass('hidden');
     requestTeam(userTeam);
   });
 }
 
-$(watchNextButton);
-$(watchStatsButton);
-$(watchNewsButton);
-$(watchForm);
-$(getCurrentTeams);
+function init() {
+  $(watchNextButton);
+  $(watchStatsButton);
+  $(watchNewsButton);
+  $(watchForm);
+  $(getCurrentTeams);
+}
+
+$(init);
